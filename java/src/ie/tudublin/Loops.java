dd7f0693f3a436a74c45b8ce6604965ce53981b3
PhilipHerweling <55754396+PhilipHerweling@users.noreply.github.com>
2020-02-12 12:01:41 +0000

    Week3

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -24,5 +24,15 @@ public class Loops extends PApplet
 	public void draw()
 	{	
 		background(0);		
+
+		/*int i = 0;
+		while (i<10)
+		{
+			i++;
+		}*/
+
+	for (int y = 10; y <= 40; y+=10)
+		line (200,y,300,y);
+
 	}
 }
