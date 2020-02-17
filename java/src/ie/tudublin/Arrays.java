48638845d932d2209be8c3901e45a74c52803750
PhilipHerweling <55754396+PhilipHerweling@users.noreply.github.com>
2020-02-17 12:58:16 +0000

    Week4

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -4,15 +4,63 @@ import processing.core.PApplet;
 
 public class Arrays extends PApplet
 {	
+
+	//float[] rainfall = new float[12];
+	float[] rainfall = {45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58};
+	String[] months = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "Decmber"};
+
+
 	public void settings()
 	{
 		size(500, 500);
 	}
 
-	public void setup() 
-	{
+	/*public void setup() 
+	{	
 		
-	}
+		for(int i = 0; i < rainfall.length; i++){
+			println(months[i]+"\t"+ rainfall[i]);
+		}
+
+		float max = 0;
+		float min = rainfall[0];
+		int cmax = 0;
+		int cmin = 0;
+
+		for(int i = 0; i < rainfall.length; i++){
+			if(rainfall[i]>max){
+				max = rainfall[i];
+				cmax = i;
+			}
+			else if(rainfall[i] < min){
+				min = rainfall[i];
+				cmin = i;
+			}
+		}
+
+		println(months[cmin] + " "+min + "\t"+ months[cmax] +" "+ max);
+		
+	}*/
+
+	void drawBarchart(){
+
+		colorMode(HSB);
+		fill(0, 300, 300);
+		int count = 0;
+		float w = width / rainfall.length;
+
+		for(int i = 0; i < months.length; i++){
+			fill(count/2, 300, 300);
+			rect(count, 500, 42, -rainfall[i]);
+			count += 42;
+
+		}
+		
+
+	}//end of barchart method
+
+	//import java.util.arrays
+	
 
 	
 
@@ -33,5 +81,6 @@ public class Arrays extends PApplet
 	{	
 		background(0);		
 		colorMode(HSB);	
+		drawBarchart();
 	}
 }
