93a019b92f51e45a9cbf52fc6ec78ad199305b3a
PhilipHerweling <55754396+PhilipHerweling@users.noreply.github.com>
2020-03-30 17:21:55 +0100

    First

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -34,6 +34,35 @@ public class StarMap extends PApplet
         }
     }
 
+    public void graph()
+	{
+
+		float gap = width * 0.1f;
+		float halfGap = gap / 2.0f;
+		colorMode(RGB);
+		stroke(0, 0, 255);
+		textAlign(CENTER, CENTER);
+		for(int i = -5 ; i <=5 ; i ++)
+		{
+			float x = map(i, -5, 5, gap, width -gap);				
+			line(x, gap, x, height - gap);
+			line(gap, x, width - gap, x);
+			fill(255);
+			text(i, x, halfGap);
+			text(i, halfGap, x);
+			
+        }
+        
+        for(Star s:stars)
+        {
+            stroke(255, 255, 0);
+            line(s.getxG(),s.getyG()-5,s.getxG(),s.getyG()+5);
+            line(s.getxG()-5, s.getyG(), s.getxG()+5, s.getyG());
+            circle(map(s.getxG(), -5, 5, gap, width -gap), map(s.getyG(), -5, 5, gap, width -gap), s.getAbsMag());
+
+        }
+	}
+
     public void printStars()
     {
         for(Star s:stars)
@@ -44,6 +73,8 @@ public class StarMap extends PApplet
 
     public void draw()
     {
+        background(0);
+        graph();
 
     }
 
